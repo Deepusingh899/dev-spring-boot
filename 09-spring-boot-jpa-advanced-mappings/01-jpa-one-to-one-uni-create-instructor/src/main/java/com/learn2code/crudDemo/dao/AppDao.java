de84d839a7b3456acb1458248088d37d60c2b70c
Deepusingh899 <deepusingh8527@gmail.com>
2024-12-17 20:46:27 +0530

    09-spring-boot-jpa-advanced-mappings

---
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni-create-instructor/src/main/java/com/learn2code/crudDemo/dao/AppDao.java
@@ -0,0 +1,8 @@
+package com.learn2code.crudDemo.dao;
+
+import com.learn2code.crudDemo.entity.Instructor;
+import org.springframework.context.annotation.Bean;
+
+public interface AppDao{
+    void save(Instructor theInstructor);
+}
